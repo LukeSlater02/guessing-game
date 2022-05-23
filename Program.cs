c7767e6744861c3a122431eba633c8051abb9774
LukeSlater02 <lukeslater101@gmail.com>
2022-05-23 13:40:07 -0500

    part 1-4

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secretNumber = 42;
+            int guesses = 1;
+            while (guesses < 5){
+            Console.WriteLine();
+            Console.WriteLine("Guess the secret number");
+            string guess = Console.ReadLine();
+
+            if (Int32.Parse(guess) == secretNumber){
+                Console.WriteLine("Correct!");
+                break;
+            } else {
+                Console.WriteLine($"Incorrect. You may try {4 - guesses} more times!");
+            }
+            guesses++;
+            }
+        }
+    }
+}
