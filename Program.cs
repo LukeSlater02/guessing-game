43962e005f7ef50f4eae20965a10a3e096194115
LukeSlater02 <lukeslater101@gmail.com>
2022-05-23 14:13:46 -0500

    Done

--- Program.cs
+++ Program.cs
@@ -7,20 +7,52 @@ namespace guessing_game
         static void Main(string[] args)
         {
             int secretNumber = 42;
-            int guesses = 1;
-            while (guesses < 5){
-            Console.WriteLine();
-            Console.WriteLine("Guess the secret number");
-            string guess = Console.ReadLine();
+            int guesses = 0;
+            double guessesMax = 0;
+            
 
-            if (Int32.Parse(guess) == secretNumber){
-                Console.WriteLine("Correct!");
-                break;
-            } else {
-                Console.WriteLine( Int32.Parse(guess) > secretNumber ?
-                    $"Incorrect, try a lower number! You may try {4 - guesses} more times!" : $"Incorrect, try a higher number! You may try {4 - guesses} more times!");
+            Console.WriteLine("Choose a difficulty level: Easy (8 guesses), Medium (6 guesses) or Hard (4 guesses), or be a filthy cheater...");
+
+            string difficulty = Console.ReadLine();
+            if (difficulty.ToLower() == "easy")
+            {
+                guessesMax = 8;
             }
-            guesses++;
+            else if (difficulty.ToLower() == "medium")
+            {
+                guessesMax = 6;
+            }
+            else if (difficulty.ToLower() == "hard")
+            {
+                guessesMax = 4;
+            }
+            else if (difficulty.ToLower() == "cheater")
+            {
+                guessesMax = Double.PositiveInfinity;
+            }
+            else
+            {
+                Console.WriteLine("Invalid difficulty input. Try again:");
+                difficulty = Console.ReadLine();
+            }
+
+            while (guesses <= guessesMax)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Guess the secret number:");
+                string guess = Console.ReadLine();
+
+                if (Int32.Parse(guess) == secretNumber)
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(Int32.Parse(guess) > secretNumber ?
+                        $"Incorrect, try a lower number! You may try {guessesMax - 1 - guesses} more times!" : $"Incorrect, try a higher number! You may try {guessesMax - 1 - guesses} more times!");
+                }
+                guesses++;
             }
         }
     }
